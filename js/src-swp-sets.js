bdfbd865800cfec15f72a67ccc881e63c93c34f4
Denilcecaes24 <164238925+Denilcecaes24@users.noreply.github.com>
2025-02-28 16:24:21 -0600

    upload files

---
+++ js/src-swp-sets.js
@@ -0,0 +1,68 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-nextt",
+        prevEl: ".swiper-button-prevv",
+      },
+
+      breakpoints: {
+        // Configuración cuando el ancho de la ventana es igual o menor a 400px
+        300: {
+            slidesPerView: 1,
+            centeredSlides: false,
+            spaceBetween: 20, // Espacio entre los slides
+        },
+        500: {
+          slidesPerView: 1,
+          centeredSlides: false,
+          spaceBetween: 20, // Espacio entre los slides
+      },
+      700: {
+        slidesPerView: 2,
+        centeredSlides: false,
+        spaceBetween: 20, // Espacio entre los slides
+    },
+
+    900: {
+        slidesPerView: 3,
+        centeredSlides: false,
+        spaceBetween: 20, // Espacio entre los slides
+    },
+  }
+  });
+
+
+  var swipertes = new Swiper(".mySwipertes", {
+    slidesPerView: 2,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-nextt",
+        prevEl: ".swiper-button-prevv",
+      },
+
+      breakpoints: {
+        // Configuración cuando el ancho de la ventana es igual o menor a 400px
+        300: {
+            slidesPerView: 1,
+            centeredSlides: false,
+            spaceBetween: 20, // Espacio entre los slides
+        },
+        500: {
+          slidesPerView: 1,
+          centeredSlides: false,
+          spaceBetween: 20, // Espacio entre los slides
+      },
+      700: {
+        slidesPerView: 1,
+        centeredSlides: false,
+        spaceBetween: 20, // Espacio entre los slides
+    },
+
+    900: {
+        slidesPerView: 2,
+        centeredSlides: false,
+        spaceBetween: 20, // Espacio entre los slides
+    },
+  }
+  });
